bca7af0cb7439a75bf2798da3cec215350f43e4f
Eldar FAYZULOV <eldar.fayzulov@bil.omu.edu.tr>
2013-03-29 18:26:20 +0200

    Create memur.java

---
+++ Kisiler/memur.java
@@ -0,0 +1,15 @@
+
+public class memur extends hoca {
+  int M_SicilNo;
+	String Birim;
+	public void Memur_Tanimla(int M_SicilNo, String Birim){
+		this.M_SicilNo = M_SicilNo;
+		this.Birim = Birim;
+	}
+	public static void Evrak_getirir(String string){
+		System.out.println("Memur evrak getirir...");
+	}
+	public static void Evrak_goturur(String string){
+		System.out.println("Memur evrak götürür...");
+	}
+}
