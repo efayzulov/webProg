6482c443b2d667f182c263abd0b382727b4d42a2
Eldar FAYZULOV <eldar.fayzulov@bil.omu.edu.tr>
2013-03-29 18:25:25 +0200

    Create hoca.java

---
+++ Kisiler/hoca.java
@@ -0,0 +1,18 @@
+
+public class hoca extends ogrenci{
+  int Sicil_No;
+	String Bolum;
+	public void Hoca_Tanimla(int Sicil_No, String Bolum){
+		this.Sicil_No = Sicil_No;
+		this.Bolum = Bolum;
+	}
+	
+	public static void ders_yap(String string){
+		System.out.println("Hoca Derse girer");
+	}
+	
+	public static void Sinav_yap(String string){
+		System.out.println("Hoca Sınav yapar");
+	}
+
+}
