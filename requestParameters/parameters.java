61182f6d91c807dc77035f76cebb5fab14edb1ac
Eldar FAYZULOV <eldar.fayzulov@bil.omu.edu.tr>
2013-04-10 00:13:02 +0300

    Create parameters.java

---
+++ requestParameters/parameters.java
@@ -0,0 +1,84 @@
+package com;
+
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class parameters
+ */
+@WebServlet("/parameters")
+public class parameters extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public parameters() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String docType =
+		"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+		"Transitional//EN\">\n";
+		String title = "Reading All Request Parameters";
+		out.println(docType +
+		    		"<HTML>\n" +
+		    		"<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" +
+		    		"<BODY>\n" +
+		    		"<H1 ALIGN=CENTER>" + title + "</H1>\n" +
+		    		"<TABLE BORDER=1 ALIGN=CENTER>\n" +
+		    		"<TR BGCOLOR=\"#FFAD00\">\n" +
+		    		"<TH>Parameter Name<TH>Parameter Value(s)");
+		Enumeration<String> paramNames = request.getParameterNames();
+		while (paramNames.hasMoreElements()) {
+			String paramName = (String) paramNames.nextElement();
+			out.print("<TR><TD>" + paramName + "\n<TD>");
+			String[] paramValues = request.getParameterValues(paramName);
+			if (paramValues.length == 1) {
+				String paramValue = paramValues[0];
+
+				if (paramValue.length() == 0)
+					out.println("<i>no value</i>");	
+				else 
+					out.print(paramValue);
+
+			}
+			else {
+				out.println("<ul>");
+
+				for(int i=0; i<paramValues.length; i++) {
+					out.println("<li>" + paramValues[i]);
+				}
+				out.println("</ul>");
+			}
+
+		}
+
+		out.println("</table>\n</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
